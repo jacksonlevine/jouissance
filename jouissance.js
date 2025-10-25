4f2feedfe0a7b4e8fa6320dd97922ca3dcb21314
Jackson Levine <jack@netbug.us>
2025-10-25 16:43:40 -0700

    add data- to our attributes to comply with html so denver's react will allow our attributes to exist

--- jouissance.js
+++ jouissance.js
@@ -3,6 +3,63 @@ import fs from 'fs';
 import { argv } from 'node:process';
 import readline from 'readline';
 
+
+
+
+
+
+function processFile() {
+    const inputfile = fs.readFileSync(inputPath);
+
+    const dom = new JSDOM(inputfile);
+    let masterscript = dom.window.document.createElement("script");
+
+    const componentTypes = [
+        {
+            htmlselector: "[data-counter]",
+            builder_func: (element, id) => {
+                element.removeAttribute("data-counter");
+                element.setAttribute(`data-counter${id}`, "");
+
+                let button = element.querySelector("[data-counterbutton]");
+                button.setAttribute("onclick", `incrementCounter${id}()`);
+                button.removeAttribute("data-counterbutton");
+
+                masterscript.innerHTML = masterscript.innerHTML + `
+                let counterval${id} = 0;
+                let el${id} = document.querySelector('[data-counter${id}]');
+                let counterdisplay${id} = el${id}.querySelector("[data-counterdisplay]");
+                function incrementCounter${id}() {
+                    counterval${id} += 1;
+                    counterdisplay${id}.innerHTML = "" + counterval${id};
+                }
+                `;
+            }
+        }
+    ];
+
+    componentTypes.forEach((scriptType) => {
+        dom.window.document.querySelectorAll(scriptType.htmlselector).forEach((element, index) => {
+            //we will use index for id
+            let id = index;
+            scriptType.builder_func(element, id);
+        });
+    });
+
+    dom.window.document.body.appendChild(masterscript);
+
+    fs.writeFileSync(outputPath, dom.serialize());
+    console.log(`Output written to ${outputPath}`);
+}
+
+
+
+
+
+
+
+
+
 let inputPath = '';
 let outputPath = '';
 let overwriteOutput = argv.includes('-f');
@@ -52,46 +109,3 @@ if (fs.existsSync(outputPath) && !overwriteOutput) {
 }
 
 
-function processFile() {
-    const inputfile = fs.readFileSync(inputPath);
-
-    const dom = new JSDOM(inputfile);
-    let masterscript = dom.window.document.createElement("script");
-
-    const componentTypes = [
-        {
-            htmlselector: "[counter]",
-            builder_func: (element, id) => {
-                element.removeAttribute("counter");
-                element.setAttribute("id", `counter${id}`);
-
-                let button = element.querySelector("[counterbutton]");
-                button.setAttribute("onclick", `incrementCounter${id}()`);
-                button.removeAttribute("counterbutton");
-
-                masterscript.innerHTML = masterscript.innerHTML + `
-                let counterval${id} = 0;
-                let el${id} = document.querySelector('#counter${id}');
-                let counterdisplay${id} = el${id}.querySelector("[counterdisplay]");
-                function incrementCounter${id}() {
-                    counterval${id} += 1;
-                    counterdisplay${id}.innerHTML = "" + counterval${id};
-                }
-                `;
-            }
-        }
-    ];
-
-    componentTypes.forEach((scriptType) => {
-        dom.window.document.querySelectorAll(scriptType.htmlselector).forEach((element, index) => {
-            //we will use index for id
-            let id = index;
-            scriptType.builder_func(element, id);
-        });
-    });
-
-    dom.window.document.body.appendChild(masterscript);
-
-    fs.writeFileSync(outputPath, dom.serialize());
-    console.log(`Output written to ${outputPath}`);
-}
